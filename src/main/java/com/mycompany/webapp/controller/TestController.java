773b17107474a30076f81a284d2adeeb18568e41
EunbinShin <eunbin851@gmail.com>
2021-01-13 17:29:08 +0900

    test

--- src/main/java/com/mycompany/webapp/controller/TestController.java
+++ src/main/java/com/mycompany/webapp/controller/TestController.java
@@ -4,11 +4,7 @@ public class TestController {
 	//Field
 	private String filed1;
 	private String filed2;
-<<<<<<< HEAD
-	private String filed4;
-=======
 	private String filed3;
->>>>>>> branch 'master' of https://github.com/EunbinShin/webapp1.git
 	//Constructor
 	//Method
 	
